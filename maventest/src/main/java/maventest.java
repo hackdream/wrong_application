9e020046ba7ea216cb6b9a7cad3e260ec7755949
maizang <275610049@qq.com>
2014-12-03 15:29:15 +0800

    <repositories>         <repository>             <id>Public online Restlet repository</id>             <url>https://raw.github.com/hackdream/maventest/master</url>         </repository>     </repositories> 这些内容尚未添加

---
+++ maventest/src/main/java/maventest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hackdream on 14-12-3.
+ */
+public class maventest {
+
+    static public void main(String argus[]){
+        System.out.println("hello maven!");
+        System.out.println("weism");
+    }
+}
