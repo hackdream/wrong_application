eaa3a2a176ed8d06325a2a60e5d9af103e02d227
maizang <275610049@qq.com>
2014-12-04 14:56:57 +0800

    <repositories>         <repository>             <id>Public online Restlet repository</id>             <url>https://raw.github.com/hackdream/maventest/master</url>         </repository>     </repositories> 这些内容尚未添加

--- maventest/src/main/java/maventest.java
+++ maventest/src/main/java/maventest.java
@@ -1,10 +1,46 @@
 /**
  * Created by hackdream on 14-12-3.
  */
+package cc.hotel;
+import java.util.*;
+import java.lang.reflect.Constructor;
+class test{
+
+   // test() {
+     //   System.out.println("无参够着");
+    //}
+    test(int i){
+        System.out.println("有参构造函数");
+        num = i;
+    }
+    static void func(){
+        System.out.println("cc.123hotel.test");
+    }
+
+    static int num = 10;
+}
 public class maventest {
 
     static public void main(String argus[]){
-        System.out.println("hello maven!");
-        System.out.println("weism");
+        test.func();
+        Class<?> temp = null;
+        System.out.println("" + test.num);
+        try {
+             temp = Class.forName("cc.hotel.test");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        Constructor<?> cons[] = null;
+        cons = temp.getConstructors();
+
+        test a = null;
+        try {
+           a = (test)cons[0].newInstance(5);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("" + a.num);
     }
 }
